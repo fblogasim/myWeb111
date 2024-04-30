c96e0c4a30fd28ceb8ecf10d84aeadd44a73bade
fblogasim <90430422+fblogasim@users.noreply.github.com>
2024-04-30 18:07:17 +1000

    minor improvements

--- service-script.js
+++ service-script.js
@@ -1,3 +1,15 @@
+const viewportSize = window.innerWidth;
+const scrollDiv = document.getElementById("scroll");
+if (viewportSize < 758){
+  scrollDiv.setAttribute("data-bs-target", "#navbar");
+  
+}
+else{
+  scrollDiv.setAttribute("data-bs-target", "#navbar1");
+  
+}
+
+
 function openNav() {
   document.getElementById("mySidebar").style.width = "100vw";
 }
